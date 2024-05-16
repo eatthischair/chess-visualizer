b7894de327fa0dd3b5c2052b6a4ab3cb805e1618
eatthischair <based.god.swag.od@gmail.com>
2024-05-16 12:51:14 -0500

    changed arg name to avoid confusion w import val

--- src/MakeElements/SetInitialBoard.js
+++ src/MakeElements/SetInitialBoard.js
@@ -1,7 +1,7 @@
 import emptyMatrix from '../HelperFunctions/MakeEmptyMatrix';
 
-const setInitialBoardPosition = emptyMatrix => {
-  let newBoard = JSON.parse(JSON.stringify(emptyMatrix));
+const setInitialBoardPosition = matrix => {
+  let newBoard = JSON.parse(JSON.stringify(matrix));
   let blackPieces = ['r1', 'n1', 'b1', 'q1', 'k1', 'b2', 'n2', 'r2'];
   let blackPawns = ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8'];
   let whitePawns = blackPawns.map(pawn => {
